39b2dfb159261116bd6e73769ac1b727547e6ff9
sanqin888 <sanqin888@gmail.com>
2025-10-03 20:46:22 -0400

    Update prisma.service.ts

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,14 +1,14 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient {
   async onModuleInit() {
     await this.$connect();
   }
 
-  // 注意：监听里不要返回 Promise，避免 no-misused-promises
-  async enableShutdownHooks(app: INestApplication) {
+  // 这里不需要 async，避免 require-await
+  enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', () => {
       void app.close();
     });
