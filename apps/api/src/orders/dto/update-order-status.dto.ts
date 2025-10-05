00959bbd32a459cb36729903bf639f556515480d
Joe Song <sanqin888@gmail.com>
2025-10-05 15:39:29 -0400

    fix(api): tidy package.json; make UpdateOrderStatusDto eslint-safe

--- apps/api/src/orders/dto/update-order-status.dto.ts
+++ apps/api/src/orders/dto/update-order-status.dto.ts
@@ -1,11 +1,6 @@
 import { IsIn } from 'class-validator';
 
-export type OrderStatus =
-  | 'pending'
-  | 'paid'
-  | 'making'
-  | 'ready'
-  | 'completed';
+export type OrderStatus = 'pending' | 'paid' | 'making' | 'ready' | 'completed';
 
 export class UpdateOrderStatusDto {
   @IsIn(['pending', 'paid', 'making', 'ready', 'completed'])
