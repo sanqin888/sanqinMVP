7440b78515d130b293752f4c3d71434381291b6c
Joe Song <sanqin888@gmail.com>
2025-10-27 22:07:29 -0400

    fix order status

--- apps/api/src/clover/clover.module.ts
+++ apps/api/src/clover/clover.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CloverService } from './clover.service';
 import { CloverController } from './clover.controller';
 import { CloverPayController } from './clover-pay.controller';
+import { OrdersModule } from '../orders/orders.module';
 
 @Module({
+  imports: [OrdersModule],
   providers: [CloverService],
   controllers: [CloverController, CloverPayController],
   exports: [CloverService],
