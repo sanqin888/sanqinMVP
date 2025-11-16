ac874d6637b963525e8bae944c0d70bf8172b993
sanqin888 <sanqin888@gmail.com>
2025-11-16 18:00:11 -0500

    feat(api): connect priority deliveries to uber direct (#136)

---
+++ apps/api/src/deliveries/deliveries.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { UberDirectService } from './uber-direct.service';
+
+@Module({
+  imports: [HttpModule],
+  providers: [UberDirectService],
+  exports: [UberDirectService],
+})
+export class DeliveriesModule {}
