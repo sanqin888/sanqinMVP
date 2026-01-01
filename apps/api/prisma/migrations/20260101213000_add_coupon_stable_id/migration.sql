7b3c100acd1d48ca6c002791362d67502325e127
Joe Song <sanqin888@gmail.com>
2026-01-01 14:17:50 -0500

    fix migration and seed-snapshot

--- apps/api/prisma/migrations/20260101213000_add_coupon_stable_id/migration.sql
+++ apps/api/prisma/migrations/20260101213000_add_coupon_stable_id/migration.sql
@@ -1,9 +1,2 @@
-ALTER TABLE "Coupon" ADD COLUMN "couponStableId" TEXT;
-
-UPDATE "Coupon"
-SET "couponStableId" = 'c' || substring(md5(id), 1, 23)
-WHERE "couponStableId" IS NULL;
-
-ALTER TABLE "Coupon" ALTER COLUMN "couponStableId" SET NOT NULL;
-
+ALTER TABLE "Coupon" ADD COLUMN "couponStableId" TEXT NOT NULL;
 CREATE UNIQUE INDEX "Coupon_couponStableId_key" ON "Coupon"("couponStableId");
