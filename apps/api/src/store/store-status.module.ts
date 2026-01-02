9c3d38532e57a0097ac3cc70431fe740cfed3d97
Joe Song <sanqin888@gmail.com>
2026-01-01 19:19:03 -0500

    fix time of store-status

--- apps/api/src/store/store-status.module.ts
+++ apps/api/src/store/store-status.module.ts
@@ -1,12 +1,12 @@
 // apps/api/src/store/store-status.module.ts
-
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { StoreStatusService } from './store-status.service';
 import { StoreStatusController } from './store-status.controller';
 
 @Module({
-  providers: [PrismaService, StoreStatusService],
+  imports: [PrismaModule],
+  providers: [StoreStatusService],
   controllers: [StoreStatusController],
 })
 export class StoreStatusModule {}
