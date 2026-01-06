3fcb7c7b95b49fcad9ab3f5ea96dd05009f329b9
sanqin888 <sanqin888@gmail.com>
2026-01-06 17:38:34 -0500

    Fix shared order exports and lint issues

--- libs/order/index.ts
+++ libs/order/index.ts
@@ -5,3 +5,21 @@ export {
   ORDER_STATUS_SEQUENCE,
   OrderStatuses,
 } from '@shared/menu';
+export {
+  ChannelSchema,
+  CreateOrderItemSchema,
+  CreateOrderSchema,
+  DeliveryDestinationSchema,
+  DeliveryTypeSchema,
+  FulfillmentTypeSchema,
+  PaymentMethodSchema,
+} from '@shared/menu';
+export type {
+  Channel,
+  CreateOrderInput,
+  CreateOrderItemInput,
+  DeliveryDestinationInput,
+  DeliveryType,
+  FulfillmentType,
+  PaymentMethod,
+} from '@shared/menu';
