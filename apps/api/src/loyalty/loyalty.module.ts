1d4f180d69fd90843b28c7ba0a39f85e4e391fa1
sanqin888 <sanqin888@gmail.com>
2026-01-09 18:36:29 -0500

    Fix loyalty module pos device guard deps

--- apps/api/src/loyalty/loyalty.module.ts
+++ apps/api/src/loyalty/loyalty.module.ts
@@ -1,11 +1,12 @@
 //apps/api/src/loyalty/loyalty.module.ts
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PosDeviceModule } from '../pos/pos-device.module';
 import { LoyaltyService } from './loyalty.service';
 import { LoyaltyController } from './loyalty.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PosDeviceModule],
   providers: [LoyaltyService],
   controllers: [LoyaltyController],
   exports: [LoyaltyService],
