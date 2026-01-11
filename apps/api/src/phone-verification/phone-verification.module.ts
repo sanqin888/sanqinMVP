31eb64c53e9e0e6dec52e35cb892df01331dafe4
Joe Song <sanqin888@gmail.com>
2026-01-11 16:28:02 -0500

    add throttler

--- apps/api/src/phone-verification/phone-verification.module.ts
+++ apps/api/src/phone-verification/phone-verification.module.ts
@@ -1,11 +1,22 @@
 // apps/api/src/phone-verification/phone-verification.module.ts
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PhoneVerificationService } from './phone-verification.service';
 import { PhoneVerificationController } from './phone-verification.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 1,
+        },
+      ],
+    }),
+  ],
   providers: [PhoneVerificationService],
   controllers: [PhoneVerificationController],
   exports: [PhoneVerificationService],
