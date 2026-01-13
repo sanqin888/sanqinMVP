603e92bed46571a9722c0bbbf02c1299a403d8e3
Joe Song <sanqin888@gmail.com>
2026-01-12 19:11:21 -0500

    remove eligibility

---
+++ apps/api/prisma/migrations/20260113001058_remove_eligibility/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `eligibility` on the `CouponProgram` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CouponProgram" DROP COLUMN "eligibility";
