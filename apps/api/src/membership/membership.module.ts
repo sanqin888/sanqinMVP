3fc83d3611ac92d0d8226a2594c5463fd8609dfa
sanqin888 <sanqin888@gmail.com>
2026-01-20 00:44:08 -0500

    Add email verification flow for marketing opt-in

--- apps/api/src/membership/membership.module.ts
+++ apps/api/src/membership/membership.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { LoyaltyModule } from '../loyalty/loyalty.module';
 import { AuthModule } from '../auth/auth.module';
+import { EmailModule } from '../email/email.module';
 import { MembershipService } from './membership.service';
 import { MembershipController } from './membership.controller';
 
 @Module({
-  imports: [PrismaModule, LoyaltyModule, AuthModule],
+  imports: [PrismaModule, LoyaltyModule, AuthModule, EmailModule],
   providers: [MembershipService],
   controllers: [MembershipController],
   exports: [MembershipService],
