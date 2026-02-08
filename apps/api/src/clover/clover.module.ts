1a0c1818bc1f086ba1d959af55091e561786afe8
sanqin888 <sanqin888@gmail.com>
2026-02-08 17:47:12 -0500

    Add Clover webhook endpoint

--- apps/api/src/clover/clover.module.ts
+++ apps/api/src/clover/clover.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CloverService } from './clover.service';
 import { CloverController } from './clover.controller';
 import { CloverPayController } from './clover-pay.controller';
+import { CloverWebhookController } from './clover-webhook.controller';
+import { CloverWebhookService } from './clover-webhook.service';
 import { OrdersModule } from '../orders/orders.module';
 import { CheckoutIntentsModule } from './checkout-intents.module';
 
 @Module({
-  imports: [OrdersModule, CheckoutIntentsModule],
-  providers: [CloverService],
-  controllers: [CloverController, CloverPayController],
+  imports: [ConfigModule, OrdersModule, CheckoutIntentsModule],
+  providers: [CloverService, CloverWebhookService],
+  controllers: [
+    CloverController,
+    CloverPayController,
+    CloverWebhookController,
+  ],
   exports: [CloverService],
 })
 export class CloverModule {}
