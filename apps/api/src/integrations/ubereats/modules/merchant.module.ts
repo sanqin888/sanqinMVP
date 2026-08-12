1a3393f6cc18a0cdacd0610eeb7db759aae4ad4d
sanqin888 <sanqin888@gmail.com>
2026-08-11 21:23:46 -0400

    refactor(api): isolate Uber Eats composition modules (#1649)

---
+++ apps/api/src/integrations/ubereats/modules/merchant.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UberEatsApplicationModule } from '../ubereats-application.module';
+
+@Module({
+  imports: [UberEatsApplicationModule],
+  exports: [UberEatsApplicationModule],
+})
+export class UberEatsMerchantModule {}
