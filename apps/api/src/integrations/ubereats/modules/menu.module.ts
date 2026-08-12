5b7a499c8df3952d6639c5a03358f6b038902712
sanqin888 <sanqin888@gmail.com>
2026-08-12 00:40:40 -0400

    refactor(api): isolate Uber Eats feature modules (#1668)

--- apps/api/src/integrations/ubereats/modules/menu.module.ts
+++ apps/api/src/integrations/ubereats/modules/menu.module.ts
@@ -1,8 +1,156 @@
 import { Module } from '@nestjs/common';
-import { UberEatsApplicationModule } from '../composition/ubereats-application.module';
+import { PrismaModule } from '../../../prisma/prisma.module';
+import { LoadUberMenuWorkflowUseCase } from '../application/menu/load-uber-menu-workflow.use-case';
+import { UberMenuDraftUseCase } from '../application/menu/uber-menu-draft.use-case';
+import { UberMenuDraftConfigUseCase } from '../application/menu/uber-menu-draft-config.use-case';
+import { UberMenuAvailabilityUseCase } from '../application/menu/uber-menu-availability.use-case';
+import { PublishUberMenuUseCase } from '../application/menu/publish-uber-menu.use-case';
+import { ConfirmUberMenuPublicationUseCase } from '../application/menu/confirm-uber-menu-publication.use-case';
+import { RecoverTimedOutMenuPublicationsUseCase } from '../application/menu/recover-timed-out-menu-publications.use-case';
+import {
+  MENU_NOTIFICATION_REPOSITORY,
+  UberMenuNotificationHandler,
+} from '../application/menu/uber-menu-notification.handler';
+import {
+  UBER_MENU_DRAFT_COMMAND_PORT,
+  UBER_MENU_DRAFT_QUERY_PORT,
+} from '../application/ports/uber-menu-draft.ports';
+import {
+  UBER_MENU_GATEWAY,
+  UBER_MENU_IMAGE_PROBE,
+  UBER_MENU_PUBLICATION_REPOSITORY,
+  UBER_MENU_PUBLISH_COMMAND,
+  UBER_MENU_SNAPSHOT_REPOSITORY,
+} from '../application/ports/uber-menu-publication.ports';
+import { UBER_MENU_UNIT_OF_WORK } from '../application/ports/uber-menu-repositories.ports';
+import {
+  UBER_MENU_AVAILABILITY_PORT,
+  UBER_MENU_DRAFT_PORT,
+} from '../application/ports/uber-use-case.ports';
+import { UberMenuAvailabilityGateway } from '../infrastructure/menu/uber-menu-availability.gateway';
+import { PrismaUberMenuUnitOfWork } from '../infrastructure/persistence/uber-menu-draft.repositories';
+import { UberMenuNotificationPrismaRepository } from '../infrastructure/persistence/uber-menu-notification-prisma.repository';
+import { UberMenuPublicationPrismaAdapter } from '../infrastructure/persistence/uber-menu-publication-prisma.adapter';
+import { UberMenuSnapshotPrismaAdapter } from '../infrastructure/persistence/uber-menu-snapshot-prisma.adapter';
+import { UberMenuDraftGateway } from '../infrastructure/persistence/uber-menu-workflow-prisma.repository';
+import {
+  UberMenuDraftCommandPrismaRepository,
+  UberMenuDraftQueryPrismaRepository,
+} from '../infrastructure/persistence/uber-menu.repository';
+import {
+  UberMenuGatewayAdapter,
+  UberMenuImageProbeAdapter,
+} from '../infrastructure/uber-api/uber-menu-publication.adapter';
+import { UberImageValidator } from '../infrastructure/uber-api/uber-image.validator';
+import { UberMenuGateway } from '../infrastructure/uber-api/uber-resource.gateways';
+import { UberEatsInternalInfrastructureModule } from './ubereats-internal-infrastructure.module';
+
+export const UBER_EATS_MENU_PROVIDERS = [
+  UberMenuGateway,
+  UberImageValidator,
+  UberMenuDraftGateway,
+  { provide: UBER_MENU_DRAFT_PORT, useExisting: UberMenuDraftGateway },
+  UberMenuAvailabilityGateway,
+  {
+    provide: UBER_MENU_AVAILABILITY_PORT,
+    useExisting: UberMenuAvailabilityGateway,
+  },
+  UberMenuDraftQueryPrismaRepository,
+  UberMenuDraftCommandPrismaRepository,
+  {
+    provide: UBER_MENU_DRAFT_QUERY_PORT,
+    useExisting: UberMenuDraftQueryPrismaRepository,
+  },
+  {
+    provide: UBER_MENU_DRAFT_COMMAND_PORT,
+    useExisting: UberMenuDraftCommandPrismaRepository,
+  },
+  PrismaUberMenuUnitOfWork,
+  { provide: UBER_MENU_UNIT_OF_WORK, useExisting: PrismaUberMenuUnitOfWork },
+  UberMenuSnapshotPrismaAdapter,
+  {
+    provide: UBER_MENU_SNAPSHOT_REPOSITORY,
+    useExisting: UberMenuSnapshotPrismaAdapter,
+  },
+  UberMenuPublicationPrismaAdapter,
+  {
+    provide: UBER_MENU_PUBLICATION_REPOSITORY,
+    useExisting: UberMenuPublicationPrismaAdapter,
+  },
+  UberMenuGatewayAdapter,
+  { provide: UBER_MENU_GATEWAY, useExisting: UberMenuGatewayAdapter },
+  UberMenuImageProbeAdapter,
+  { provide: UBER_MENU_IMAGE_PROBE, useExisting: UberMenuImageProbeAdapter },
+  UberMenuNotificationPrismaRepository,
+  {
+    provide: MENU_NOTIFICATION_REPOSITORY,
+    useExisting: UberMenuNotificationPrismaRepository,
+  },
+  {
+    provide: UberMenuDraftUseCase,
+    inject: [UBER_MENU_DRAFT_PORT],
+    useFactory: (drafts) => new UberMenuDraftUseCase(drafts),
+  },
+  {
+    provide: UberMenuDraftConfigUseCase,
+    inject: [UBER_MENU_DRAFT_QUERY_PORT, UBER_MENU_DRAFT_COMMAND_PORT],
+    useFactory: (queries, commands) =>
+      new UberMenuDraftConfigUseCase(queries, commands),
+  },
+  {
+    provide: LoadUberMenuWorkflowUseCase,
+    inject: [UBER_MENU_UNIT_OF_WORK],
+    useFactory: (unitOfWork) => new LoadUberMenuWorkflowUseCase(unitOfWork),
+  },
+  {
+    provide: PublishUberMenuUseCase,
+    inject: [
+      UBER_MENU_SNAPSHOT_REPOSITORY,
+      UBER_MENU_PUBLICATION_REPOSITORY,
+      UBER_MENU_GATEWAY,
+      UBER_MENU_IMAGE_PROBE,
+    ],
+    useFactory: (snapshots, publications, gateway, images) =>
+      new PublishUberMenuUseCase(snapshots, publications, gateway, images),
+  },
+  { provide: UBER_MENU_PUBLISH_COMMAND, useExisting: PublishUberMenuUseCase },
+  {
+    provide: ConfirmUberMenuPublicationUseCase,
+    inject: [UBER_MENU_PUBLICATION_REPOSITORY, UBER_MENU_GATEWAY],
+    useFactory: (publications, gateway) =>
+      new ConfirmUberMenuPublicationUseCase(publications, gateway),
+  },
+  {
+    provide: RecoverTimedOutMenuPublicationsUseCase,
+    inject: [UBER_MENU_PUBLICATION_REPOSITORY],
+    useFactory: (publications) =>
+      new RecoverTimedOutMenuPublicationsUseCase(publications),
+  },
+  {
+    provide: UberMenuNotificationHandler,
+    inject: [MENU_NOTIFICATION_REPOSITORY],
+    useFactory: (repository) => new UberMenuNotificationHandler(repository),
+  },
+  {
+    provide: UberMenuAvailabilityUseCase,
+    inject: [UBER_MENU_AVAILABILITY_PORT],
+    useFactory: (availability) => new UberMenuAvailabilityUseCase(availability),
+  },
+];
+
+export const UBER_EATS_MENU_EXPORTS = [
+  UberMenuDraftUseCase,
+  UberMenuDraftConfigUseCase,
+  PublishUberMenuUseCase,
+  ConfirmUberMenuPublicationUseCase,
+  RecoverTimedOutMenuPublicationsUseCase,
+  UberMenuNotificationHandler,
+  UberMenuAvailabilityUseCase,
+];
 
 @Module({
-  imports: [UberEatsApplicationModule],
-  exports: [UberEatsApplicationModule],
+  imports: [PrismaModule, UberEatsInternalInfrastructureModule],
+  providers: UBER_EATS_MENU_PROVIDERS,
+  exports: UBER_EATS_MENU_EXPORTS,
 })
 export class UberEatsMenuModule {}
--- apps/api/src/integrations/ubereats/modules/merchant.module.ts
+++ apps/api/src/integrations/ubereats/modules/merchant.module.ts
@@ -1,8 +1,155 @@
 import { Module } from '@nestjs/common';
-import { UberEatsApplicationModule } from '../composition/ubereats-application.module';
+import { PrismaModule } from '../../../prisma/prisma.module';
+import {
+  CompleteUberOAuthUseCase,
+  StartUberOAuthUseCase,
+} from '../application/merchant/uber-merchant-oauth.service';
+import {
+  DeprovisionUberStoreUseCase,
+  ProvisionUberStoreUseCase,
+  SyncUberStoreStatusUseCase,
+} from '../application/merchant/uber-merchant-provisioning.service';
+import {
+  DiscoverUberStoresUseCase,
+  MapUberStoreUseCase,
+} from '../application/merchant/uber-merchant-store-mapping.service';
+import { HandleUberMerchantWebhookHandler } from '../application/merchant/uber-merchant-webhook.handler';
+import {
+  UBER_MERCHANT_API,
+  UBER_OAUTH_TOKEN,
+  UBER_STORE_API,
+} from '../application/ports/uber-api.ports';
+import {
+  UBER_MERCHANT_CONNECTION_REPOSITORY,
+  UBER_OAUTH_STATE_REPOSITORY,
+  UBER_OPERATIONS_ALERT_REPOSITORY,
+  UBER_STORE_MAPPING_REPOSITORY,
+} from '../application/ports/uber-persistence.ports';
+import {
+  UBER_TELEMETRY_PORT,
+  UBER_WEBHOOK_INBOX_PORT,
+} from '../application/ports/uber-order-processing.ports';
+import {
+  UberMerchantConnectionPrismaAdapter,
+  UberOAuthStatePrismaAdapter,
+  UberOperationsAlertPrismaAdapter,
+  UberStoreMappingPrismaAdapter,
+} from '../infrastructure/persistence/uber-merchant-persistence.adapter';
+import {
+  UberMerchantApiAdapter,
+  UberOAuthTokenAdapter,
+} from '../infrastructure/uber-api/uber-merchant-api.adapter';
+import {
+  UberMerchantResourceGateway,
+  UberStoreGateway,
+} from '../infrastructure/uber-api/uber-resource.gateways';
+import { UberEatsInternalInfrastructureModule } from './ubereats-internal-infrastructure.module';
+
+export const UBER_EATS_MERCHANT_PROVIDERS = [
+  UberMerchantResourceGateway,
+  UberStoreGateway,
+  UberOAuthTokenAdapter,
+  { provide: UBER_OAUTH_TOKEN, useExisting: UberOAuthTokenAdapter },
+  UberMerchantApiAdapter,
+  { provide: UBER_MERCHANT_API, useExisting: UberMerchantApiAdapter },
+  { provide: UBER_STORE_API, useExisting: UberMerchantApiAdapter },
+  UberOAuthStatePrismaAdapter,
+  {
+    provide: UBER_OAUTH_STATE_REPOSITORY,
+    useExisting: UberOAuthStatePrismaAdapter,
+  },
+  UberMerchantConnectionPrismaAdapter,
+  {
+    provide: UBER_MERCHANT_CONNECTION_REPOSITORY,
+    useExisting: UberMerchantConnectionPrismaAdapter,
+  },
+  UberStoreMappingPrismaAdapter,
+  {
+    provide: UBER_STORE_MAPPING_REPOSITORY,
+    useExisting: UberStoreMappingPrismaAdapter,
+  },
+  UberOperationsAlertPrismaAdapter,
+  {
+    provide: UBER_OPERATIONS_ALERT_REPOSITORY,
+    useExisting: UberOperationsAlertPrismaAdapter,
+  },
+  {
+    provide: StartUberOAuthUseCase,
+    inject: [UBER_OAUTH_TOKEN, UBER_OAUTH_STATE_REPOSITORY],
+    useFactory: (tokens, states) => new StartUberOAuthUseCase(tokens, states),
+  },
+  {
+    provide: CompleteUberOAuthUseCase,
+    inject: [
+      UBER_OAUTH_TOKEN,
+      UBER_OAUTH_STATE_REPOSITORY,
+      UBER_MERCHANT_CONNECTION_REPOSITORY,
+    ],
+    useFactory: (tokens, states, connections) =>
+      new CompleteUberOAuthUseCase(tokens, states, connections),
+  },
+  {
+    provide: DiscoverUberStoresUseCase,
+    inject: [
+      UBER_MERCHANT_API,
+      UBER_OAUTH_TOKEN,
+      UBER_MERCHANT_CONNECTION_REPOSITORY,
+      UBER_STORE_MAPPING_REPOSITORY,
+    ],
+    useFactory: (api, tokens, connections, mappings) =>
+      new DiscoverUberStoresUseCase(api, tokens, connections, mappings),
+  },
+  {
+    provide: MapUberStoreUseCase,
+    inject: [UBER_STORE_MAPPING_REPOSITORY],
+    useFactory: (mappings) => new MapUberStoreUseCase(mappings),
+  },
+  {
+    provide: ProvisionUberStoreUseCase,
+    inject: [
+      UBER_STORE_API,
+      UBER_MERCHANT_CONNECTION_REPOSITORY,
+      UBER_STORE_MAPPING_REPOSITORY,
+    ],
+    useFactory: (api, connections, mappings) =>
+      new ProvisionUberStoreUseCase(api, connections, mappings),
+  },
+  {
+    provide: DeprovisionUberStoreUseCase,
+    useFactory: () => new DeprovisionUberStoreUseCase(),
+  },
+  {
+    provide: SyncUberStoreStatusUseCase,
+    inject: [
+      UBER_STORE_API,
+      UBER_STORE_MAPPING_REPOSITORY,
+      UBER_OPERATIONS_ALERT_REPOSITORY,
+    ],
+    useFactory: (api, mappings, alerts) =>
+      new SyncUberStoreStatusUseCase(api, mappings, alerts),
+  },
+  {
+    provide: HandleUberMerchantWebhookHandler,
+    inject: [UBER_WEBHOOK_INBOX_PORT, UBER_TELEMETRY_PORT],
+    useFactory: (inbox, telemetry) =>
+      new HandleUberMerchantWebhookHandler(inbox, telemetry),
+  },
+];
+
+export const UBER_EATS_MERCHANT_EXPORTS = [
+  StartUberOAuthUseCase,
+  CompleteUberOAuthUseCase,
+  DiscoverUberStoresUseCase,
+  MapUberStoreUseCase,
+  ProvisionUberStoreUseCase,
+  DeprovisionUberStoreUseCase,
+  SyncUberStoreStatusUseCase,
+  HandleUberMerchantWebhookHandler,
+];
 
 @Module({
-  imports: [UberEatsApplicationModule],
-  exports: [UberEatsApplicationModule],
+  imports: [PrismaModule, UberEatsInternalInfrastructureModule],
+  providers: UBER_EATS_MERCHANT_PROVIDERS,
+  exports: UBER_EATS_MERCHANT_EXPORTS,
 })
 export class UberEatsMerchantModule {}
