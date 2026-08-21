ad6e14c8abfe722959df95bd266131a117906638
sanqin888 <sanqin888@gmail.com>
2026-08-21 08:58:16 -0400

    fix(api): remove duplicate POS order type declarations (#1868)

--- apps/api/src/pos/pos-orders.controller.ts
+++ apps/api/src/pos/pos-orders.controller.ts
@@ -61,14 +61,6 @@ type PosBoardOrderDto = OrderDto & {
   fulfillmentTiming: OrderFulfillmentTiming;
 };
 
-type PosDeviceRequest = Request & {
-  posDevice?: { storeId: string };
-};
-
-type PosBoardOrderDto = OrderDto & {
-  fulfillmentTiming: OrderFulfillmentTiming;
-};
-
 class CreateFullRefundDto {
   @IsString()
   reason!: string;
